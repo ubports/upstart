f43bdf3fbd4bdef7898fdd12b3c3f9259273bd39
Scott James Remnant <scott@netsplit.com>
2006-08-27 18:20:29 +0100

    * init/main.c (main): Stop handling SIGTERM, we never want people to kill init.  Handle SIGINT and SIGWINCH through the ordinary handler and SIGSEGV through a direct handler. (segv_handler): Write a sensible core dump handler, we use a child to dump core while we carry on in the parent hopefully stepping over the bad instruction. (cad_handler): Generate the control-alt-delete event. (kbd_handler): Generate the kbdrequest event.

--- init/main.c
+++ init/main.c
@@ -23,6 +23,15 @@
 #endif /* HAVE_CONFIG_H */
 
 
+#include <linux/kd.h>
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/ioctl.h>
+#include <sys/reboot.h>
+#include <sys/resource.h>
+
+#include <signal.h>
 #include <stdlib.h>
 #include <syslog.h>
 #include <unistd.h>
@@ -43,6 +52,12 @@
 #include "cfgfile.h"
 
 
+/* Prototypes for static functions */
+static void segv_handler (int signum);
+static void cad_handler  (void *data, NihSignal *signal);
+static void kbd_handler  (void *data, NihSignal *signal);
+
+
 int
 main (int   argc,
       char *argv[])
@@ -56,25 +71,6 @@ main (int   argc,
 	nih_log_set_priority (NIH_LOG_DEBUG);
 	nih_log_set_logger (nih_logger_syslog);
 
-
-	/* Reset the signal state and install the signal handler for those
-	 * signals we actually want to catch; this also sets those that
-	 * can be sent to us, because we're special
-	 */
-	nih_signal_reset ();
-	nih_signal_set_handler (SIGALRM, nih_signal_handler);
-	nih_signal_set_handler (SIGTERM, nih_signal_handler);
-	nih_signal_set_handler (SIGHUP,  nih_signal_handler);
-
-	nih_signal_add_callback (NULL, SIGTERM, nih_main_term_signal, NULL);
-
-	/* Reap all children that die */
-	nih_child_add_watch (NULL, -1, job_child_reaper, NULL);
-
-	/* Process the event queue every time through the main loop */
-	nih_main_loop_add_func (NULL, event_queue_run, NULL);
-
-
 	/* Close any file descriptors we inherited, and open the console
 	 * device instead
 	 */
@@ -83,7 +79,37 @@ main (int   argc,
 
 	process_setup_console (CONSOLE_OUTPUT);
 
-	setenv ("PATH", PATH, TRUE);
+
+	/* Reset the signal state and install the signal handler for those
+	 * signals we actually want to catch; this also sets those that
+	 * can be sent to us, because we're special
+	 */
+	nih_signal_reset ();
+	nih_signal_set_handler (SIGALRM,  nih_signal_handler);
+	nih_signal_set_handler (SIGHUP,   nih_signal_handler);
+	nih_signal_set_handler (SIGINT,   nih_signal_handler);
+	nih_signal_set_handler (SIGWINCH, nih_signal_handler);
+	nih_signal_set_handler (SIGSEGV,  segv_handler);
+
+	/* Ask the kernel to send us SIGINT when control-alt-delete is
+	 * pressed; generate an event with the same name.
+	 */
+	reboot (RB_DISABLE_CAD);
+	nih_signal_add_callback (NULL, SIGINT, cad_handler, NULL);
+
+	/* Ask the kernel to send us SIGWINCH when alt-uparrow is pressed;
+	 * generate a kbdrequest event.
+	 */
+	ioctl (0, KDSIGACCEPT, SIGWINCH);
+	nih_signal_add_callback (NULL, SIGWINCH, kbd_handler, NULL);
+
+
+	/* Reap all children that die */
+	nih_child_add_watch (NULL, -1, job_child_reaper, NULL);
+
+	/* Process the event queue every time through the main loop */
+	nih_main_loop_add_func (NULL, event_queue_run, NULL);
+
 
 	/* Become session and process group leader (should be already,
 	 * but you never know what initramfs did
@@ -96,6 +122,9 @@ main (int   argc,
 	/* Read configuration */
 	cfg_watch_dir (NULL, CFG_DIR, NULL);
 
+	/* Set the PATH environment variable */
+	setenv ("PATH", PATH, TRUE);
+
 
 	/* Generate and run the startup event */
 	event_queue_edge ("startup");
@@ -105,4 +134,94 @@ main (int   argc,
 	ret = nih_main_loop ();
 
 	return ret;
+}
+
+
+/**
+ * segv_handler:
+ * @signum:
+ *
+ * Handle receiving the SEGV signal, usually caused by one of our own
+ * mistakes.  We deal with it by dumping core in a child process and
+ * just carrying on in the parent.
+ **/
+static void
+segv_handler (int signum)
+{
+	pid_t pid;
+
+	pid = fork ();
+	if (pid == 0) {
+		struct sigaction act;
+		struct rlimit    limit;
+		sigset_t         mask;
+
+		/* Mask out all signals */
+		sigfillset (&mask);
+		sigprocmask (SIG_SETMASK, &mask, NULL);
+
+		/* Set the SEGV handler to the default so core is dumped */
+		act.sa_handler = SIG_DFL;
+		act.sa_flags = 0;
+		sigemptyset (&act.sa_mask);
+		sigaction (SIGSEGV, &act, NULL);
+
+		/* Dump in the root directory */
+		chdir ("/");
+
+		/* Don't limit the core dump size */
+		limit.rlim_cur = RLIM_INFINITY;
+		limit.rlim_max = RLIM_INFINITY;
+		setrlimit (RLIMIT_CORE, &limit);
+
+		/* Raise the signal */
+		raise (SIGSEGV);
+
+		/* Unmask so that we receive it */
+		sigdelset (&mask, SIGSEGV);
+		sigprocmask (SIG_SETMASK, &mask, NULL);
+
+		/* Wait for death */
+		pause ();
+		exit (0);
+	} else if (pid > 0) {
+		/* Wait for the core to be generated */
+		waitpid (pid, NULL, 0);
+
+		nih_error (_("Caught segmentation fault, core dumped"));
+	} else {
+		nih_error (_("Caught segmentation fault, unable to dump core"));
+	}
+}
+
+/**
+ * cad_handler:
+ * @data: unused,
+ * @signal: signal that called this handler.
+ *
+ * Handle having recieved the SIGINT signal, sent to us when somebody
+ * presses Ctrl-Alt-Delete on the console.  We just generate a
+ * control-alt-delete event.
+ **/
+static void
+cad_handler (void      *data,
+	     NihSignal *signal)
+{
+	event_queue_edge ("control-alt-delete");
+}
+
+/**
+ * kbd_handler:
+ * @data: unused,
+ * @signal: signal that called this handler.
+ *
+ * Handle having recieved the SIGWINCH signal, sent to us when somebody
+ * presses Alt-UpArrow on the console.  We just generate a
+ * kbdrequest event.
+ **/
+static void
+kbd_handler (void      *data,
+	     NihSignal *signal)
+{
+	event_queue_edge ("kbdrequest");
 }
