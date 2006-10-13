48622985561cdf41dc6c662e20bf82a0d8520887
Scott James Remnant <scott@netsplit.com>
2006-10-13 15:15:50 +0100

    * util/initctl.c: Rewrite using nih_command_parser. * util/man/initctl.8: Improve. * util/start.c: Remove, replaced by initctl. * util/man/start.8: Remove, replaced by initctl. * util/Makefile.am (sbin_PROGRAMS): Drop start, now just a symlink to initctl. (dist_man_MANS): Drop start.8, now a symlink to initctl.8 (install-exec-hook): Make symlinks to initctl, add start (install-data-hook): Make symlinks to initctl.8, add start.8

--- util/initctl.c
+++ util/initctl.c
@@ -29,13 +29,402 @@
 #include <unistd.h>
 
 #include <nih/macros.h>
+#include <nih/alloc.h>
 #include <nih/main.h>
 #include <nih/option.h>
+#include <nih/command.h>
 #include <nih/logging.h>
 #include <nih/error.h>
 
 #include <upstart/job.h>
 #include <upstart/control.h>
+
+
+/* Prototypes for static functions */
+static int  open_socket      (void);
+static void print_job_status (UpstartMsg *reply);
+static void print_event      (UpstartMsg *reply);
+
+
+/**
+ * start_options:
+ *
+ * Command-line options accepted for the start, stop and status commands.
+ **/
+static NihOption start_options[] = {
+	NIH_OPTION_LAST
+};
+
+/**
+ * start_action:
+ * @command: command invoked for,
+ * @args: arguments passed.
+ *
+ * Function invoked when the start, stop or status command is run.  The
+ * arguments are expected to be a list of jobs that should have their status
+ * changed.
+ *
+ * Returns: zero on success, exit status on error.
+ **/
+static int
+start_action (NihCommand   *command,
+	      char * const *args)
+{
+	char * const *arg;
+	int           sock;
+
+	nih_assert (command != NULL);
+	nih_assert (args != NULL);
+
+	if (! args[0]) {
+		fprintf (stderr, _("%s: missing job name"), program_name);
+		nih_main_suggest_help ();
+		exit (1);
+	}
+
+	sock = open_socket ();
+
+	/* Iterate job names */
+	for (arg = args; *arg; arg++) {
+		UpstartMsg msg, *reply;
+
+		/* Build the message to send */
+		if (! strcmp (command->command, "start")) {
+			msg.type = UPSTART_JOB_START;
+			msg.job_start.name = *arg;
+		} else if (! strcmp (command->command, "stop")) {
+			msg.type = UPSTART_JOB_STOP;
+			msg.job_stop.name = *arg;
+			break;
+		} else if (! strcmp (command->command, "status")) {
+			msg.type = UPSTART_JOB_QUERY;
+			msg.job_stop.name = *arg;
+			break;
+		}
+
+		/* Send the message */
+		if (upstart_send_msg (sock, &msg) < 0) {
+			NihError *err;
+
+			err = nih_error_get ();
+			nih_error (_("Unable to send message: %s"),
+				   err->message);
+			exit (1);
+		}
+
+		/* Wait for the reply */
+		reply = upstart_recv_msg (NULL, sock, NULL);
+		if (! reply) {
+			NihError *err;
+
+			err = nih_error_get ();
+			nih_error (_("Error receiving message: %s"),
+				   err->message);
+			exit (1);
+		}
+
+		switch (reply->type) {
+		case UPSTART_JOB_STATUS:
+			print_job_status (reply);
+			break;
+		case UPSTART_JOB_UNKNOWN:
+			nih_warn (_("unknown job: %s\n"),
+				  reply->job_unknown.name);
+			break;
+		default:
+			nih_warn (_("unexpected reply (type %d)\n"),
+				  reply->type);
+		}
+
+		nih_free (reply);
+	}
+
+	return 0;
+}
+
+
+/**
+ * list_options:
+ *
+ * Command-line options accepted for the list command.
+ **/
+static NihOption list_options[] = {
+	NIH_OPTION_LAST
+};
+
+/**
+ * list_action:
+ * @command: command invoked for,
+ * @args: arguments passed.
+ *
+ * Function invoked when the list command is run.  No arguments are permitted.
+ *
+ * Returns: zero on success, exit status on error.
+ **/
+static int
+list_action (NihCommand   *command,
+	     char * const *args)
+{
+	UpstartMsg msg;
+	int        sock, receive_replies;
+
+	nih_assert (command != NULL);
+	nih_assert (args != NULL);
+
+	sock = open_socket ();
+
+	msg.type = UPSTART_JOB_LIST;
+
+	/* Send the message */
+	if (upstart_send_msg (sock, &msg) < 0) {
+		NihError *err;
+
+		err = nih_error_get ();
+		nih_error (_("Unable to send message: %s"), err->message);
+		exit (1);
+	}
+
+	/* Receive all replies */
+	receive_replies = 1;
+	while (receive_replies) {
+		UpstartMsg *reply;
+
+		reply = upstart_recv_msg (NULL, sock, NULL);
+		if (! reply) {
+			NihError *err;
+
+			err = nih_error_get ();
+			nih_error (_("Error receiving message: %s"),
+				   err->message);
+			exit (1);
+		}
+
+		switch (reply->type) {
+		case UPSTART_JOB_STATUS:
+			print_job_status (reply);
+			break;
+		case UPSTART_JOB_LIST_END:
+			receive_replies = 0;
+			break;
+		case UPSTART_JOB_UNKNOWN:
+			nih_warn (_("unknown job: %s\n"),
+				  reply->job_unknown.name);
+			break;
+		default:
+			nih_warn (_("unexpected reply (type %d)\n"),
+				  reply->type);
+		}
+
+		nih_free (reply);
+	}
+
+	return 0;
+}
+
+
+/**
+ * emit_options:
+ *
+ * Command-line options accepted for the emit and shutdown commands.
+ **/
+static NihOption emit_options[] = {
+	NIH_OPTION_LAST
+};
+
+/**
+ * emit_action:
+ * @command: command invoked for,
+ * @args: arguments passed.
+ *
+ * Function invoked when the emit or shutdown command is run.  An event name
+ * is expected.
+ *
+ * Returns: zero on success, exit status on error.
+ **/
+static int
+emit_action (NihCommand   *command,
+	     char * const *args)
+{
+	UpstartMsg msg;
+	int        sock;
+
+	nih_assert (command != NULL);
+	nih_assert (args != NULL);
+
+	if (! args[0]) {
+		fprintf (stderr, _("%s: missing event name"), program_name);
+		nih_main_suggest_help ();
+		exit (1);
+	}
+
+	sock = open_socket ();
+
+	if (! strcmp (command->command, "emit")) {
+		msg.type = UPSTART_EVENT_QUEUE;
+	} else if (! strcmp (command->command, "trigger")) {
+		msg.type = UPSTART_EVENT_QUEUE;
+	} else if (! strcmp (command->command, "shutdown")) {
+		msg.type = UPSTART_SHUTDOWN;
+	}
+	msg.event_queue.name = args[0];
+
+	/* Send the message */
+	if (upstart_send_msg (sock, &msg) < 0) {
+		NihError *err;
+
+		err = nih_error_get ();
+		nih_error (_("Unable to send message: %s"), err->message);
+		exit (1);
+	}
+
+	return 0;
+}
+
+
+/**
+ * jobs_options:
+ *
+ * Command-line options accepted for the jobs command.
+ **/
+static NihOption jobs_options[] = {
+	NIH_OPTION_LAST
+};
+
+/**
+ * jobs_action:
+ * @command: command invoked for,
+ * @args: arguments passed.
+ *
+ * Function invoked when the jobs command is run.  No arguments are
+ * expected.
+ *
+ * Returns: zero on success, exit status on error.
+ **/
+static int
+jobs_action (NihCommand   *command,
+	     char * const *args)
+{
+	UpstartMsg msg;
+	int        sock;
+
+	nih_assert (command != NULL);
+	nih_assert (args != NULL);
+
+	sock = open_socket ();
+
+	msg.type = UPSTART_WATCH_JOBS;
+
+	/* Send the message */
+	if (upstart_send_msg (sock, &msg) < 0) {
+		NihError *err;
+
+		err = nih_error_get ();
+		nih_error (_("Unable to send message: %s"),
+			   err->message);
+		exit (1);
+	}
+
+	/* Receive all replies */
+	for (;;) {
+		UpstartMsg *reply;
+
+		reply = upstart_recv_msg (NULL, sock, NULL);
+		if (! reply) {
+			NihError *err;
+
+			err = nih_error_get ();
+			nih_error (_("Error receiving message: %s"),
+				   err->message);
+			exit (1);
+		}
+
+		switch (reply->type) {
+		case UPSTART_JOB_STATUS:
+			print_job_status (reply);
+			break;
+		default:
+			nih_warn (_("unexpected reply (type %d)\n"),
+				  reply->type);
+		}
+
+		nih_free (reply);
+	}
+
+	return 0;
+}
+
+
+/**
+ * events_options:
+ *
+ * Command-line options accepted for the events command.
+ **/
+static NihOption events_options[] = {
+	NIH_OPTION_LAST
+};
+
+/**
+ * events_action:
+ * @command: command invoked for,
+ * @args: arguments passed.
+ *
+ * Function invoked when the events command is run.  No arguments are
+ * expected.
+ *
+ * Returns: zero on success, exit status on error.
+ **/
+static int
+events_action (NihCommand   *command,
+	       char * const *args)
+{
+	UpstartMsg msg;
+	int        sock;
+
+	nih_assert (command != NULL);
+	nih_assert (args != NULL);
+
+	sock = open_socket ();
+
+	msg.type = UPSTART_WATCH_EVENTS;
+
+	/* Send the message */
+	if (upstart_send_msg (sock, &msg) < 0) {
+		NihError *err;
+
+		err = nih_error_get ();
+		nih_error (_("Unable to send message: %s"),
+			   err->message);
+		exit (1);
+	}
+
+	/* Receive all replies */
+	for (;;) {
+		UpstartMsg *reply;
+
+		reply = upstart_recv_msg (NULL, sock, NULL);
+		if (! reply) {
+			NihError *err;
+
+			err = nih_error_get ();
+			nih_error (_("Error receiving message: %s"),
+				   err->message);
+			exit (1);
+		}
+
+		switch (reply->type) {
+		case UPSTART_EVENT:
+			print_event (reply);
+			break;
+		default:
+			nih_warn (_("unexpected reply (type %d)\n"),
+				  reply->type);
+		}
+
+		nih_free (reply);
+	}
+
+	return 0;
+}
 
 
 /**
@@ -48,20 +437,102 @@ static NihOption options[] = {
 };
 
 
+/**
+ * job_group:
+ *
+ * Group of commands related to jobs.
+ **/
+static NihCommandGroup job_commands = { N_("Job") };
+
+/**
+ * event_group:
+ *
+ * Group of commands related to events.
+ **/
+static NihCommandGroup event_commands = { N_("Event") };
+
+/**
+ * commands:
+ *
+ * Commands accepts as the first non-option argument, or program name.
+ **/
+static NihCommand commands[] = {
+	{ "start", N_("JOB..."),
+	  N_("Start jobs."),
+	  N_("JOB is one or more job names that are to be started."),
+	  &job_commands, start_options, start_action },
+
+	{ "stop", N_("JOB..."),
+	  N_("Stop jobs."),
+	  N_("JOB is one or more job names that are to be stopped."),
+	  &job_commands, start_options, start_action },
+
+	{ "status", N_("JOB..."),
+	  N_("Query status of jobs."),
+	  N_("JOB is one or more job names that are to be queried."),
+	  &job_commands, start_options, start_action },
+
+	{ "list", NULL,
+	  N_("List known jobs."),
+	  NULL,
+	  &job_commands, list_options, list_action },
+
+	{ "emit", N_("EVENT"),
+	  N_("Emit an event."),
+	  N_("EVENT is the name of an event the init daemon should emit."),
+	  &event_commands, emit_options, emit_action },
+
+	{ "trigger", N_("EVENT"),
+	  NULL,
+	  NULL,
+	  &event_commands, emit_options, emit_action },
+
+	{ "jobs", NULL,
+	  N_("Receive notification of job state changes."),
+	  NULL,
+	  &job_commands, jobs_options, jobs_action },
+
+	{ "events", NULL,
+	  N_("Receive notification of emitted events."),
+	  NULL,
+	  &event_commands, events_options, events_action },
+
+	{ "shutdown", N_("EVENT"),
+	  N_("Emit a shutdown event."),
+	  N_("EVENT is the name of an event the init daemon should emit "
+	     "after the shutdown event has been emitted."),
+	  &event_commands, emit_options, emit_action },
+
+	NIH_COMMAND_LAST
+};
+
+
 int
 main (int   argc,
       char *argv[])
 {
-	UpstartMsg   msg;
-	int          sock, expect_reply = 1;
-	char       **args;
+	int ret;
 
 	nih_main_init (argv[0]);
-	nih_option_set_usage ("CMD [ARG]...");
 
-	args = nih_option_parser (NULL, argc, argv, options, TRUE);
-	if (! args)
-		exit (1);
+	ret = nih_command_parser (NULL, argc, argv, options, commands);
+
+	return ret;
+}
+
+
+/**
+ * open_socket:
+ *
+ * Open a connection to the upstart daemon, this will exit the process if
+ * not successful.
+ *
+ * Returns: socket or exits.
+ **/
+static int
+open_socket (void)
+{
+	int sock;
 
 	/* Check we're root */
 	setuid (geteuid ());
@@ -81,175 +552,53 @@ main (int   argc,
 		exit (1);
 	}
 
-	/* First argument is the command */
-	if (args[0] == NULL) {
-		fprintf (stderr, _("%s: missing command\n"), program_name);
-		nih_main_suggest_help ();
-		exit (1);
-	} else if (! strcmp (args[0], "start")) {
-		if (args[1] == NULL) {
-			fprintf (stderr, _("%s: missing argument\n"),
-				 program_name);
-			nih_main_suggest_help ();
-			exit (1);
-		} else if (args[2] != NULL) {
-			fprintf (stderr, _("%s: unexpected argument\n"),
-				 program_name);
-			nih_main_suggest_help ();
-			exit (1);
-		}
+	return sock;
+}
 
-		msg.type = UPSTART_JOB_START;
-		msg.job_start.name = args[1];
-	} else if (! strcmp (args[0], "stop")) {
-		if (args[1] == NULL) {
-			fprintf (stderr, _("%s: missing argument\n"),
-				 program_name);
-			nih_main_suggest_help ();
-			exit (1);
-		} else if (args[2] != NULL) {
-			fprintf (stderr, _("%s: unexpected argument\n"),
-				 program_name);
-			nih_main_suggest_help ();
-			exit (1);
-		}
 
-		msg.type = UPSTART_JOB_STOP;
-		msg.job_stop.name = args[1];
-	} else if (! strcmp (args[0], "status")) {
-		if (args[1] == NULL) {
-			fprintf (stderr, _("%s: missing argument\n"),
-				 program_name);
-			nih_main_suggest_help ();
-			exit (1);
-		} else if (args[2] != NULL) {
-			fprintf (stderr, _("%s: unexpected argument\n"),
-				 program_name);
-			nih_main_suggest_help ();
-			exit (1);
-		}
+/**
+ * print_job_status:
+ * @reply: message received.
+ *
+ * Output job status information received from the init daemon.
+ **/
+static void
+print_job_status (UpstartMsg *reply)
+{
+	nih_assert (reply != NULL);
+	nih_assert (reply->type == UPSTART_JOB_STATUS);
 
-		msg.type = UPSTART_JOB_QUERY;
-		msg.job_query.name = args[1];
-	} else if (! strcmp (args[0], "list")) {
-		if (args[1] != NULL) {
-			fprintf (stderr, _("%s: unexpected argument\n"),
-				 program_name);
-			nih_main_suggest_help ();
-			exit (1);
-		}
+	nih_message ("%s (%s) %s", reply->job_status.name,
+		     job_goal_name (reply->job_status.goal),
+		     job_state_name (reply->job_status.state));
 
-		msg.type = UPSTART_JOB_LIST;
-		expect_reply = -1;
-	} else if (! strcmp (args[0], "trigger")) {
-		if (args[1] == NULL) {
-			fprintf (stderr, _("%s: missing argument\n"),
-				 program_name);
-			nih_main_suggest_help ();
-			exit (1);
-		}
+	if (reply->job_status.state == JOB_WAITING) {
+		nih_message ("\n");
 
-		msg.type = UPSTART_EVENT_QUEUE;
-		msg.event_queue.name = args[1];
-		expect_reply = 0;
-	} else if (! strcmp (args[0], "jobs")) {
-		if (args[1] != NULL) {
-			fprintf (stderr, _("%s: unexpected argument\n"),
-				 program_name);
-			nih_main_suggest_help ();
-			exit (1);
-		}
-
-		msg.type = UPSTART_WATCH_JOBS;
-		expect_reply = -1;
-	} else if (! strcmp (args[0], "events")) {
-		if (args[1] != NULL) {
-			fprintf (stderr, _("%s: unexpected argument\n"),
-				 program_name);
-			nih_main_suggest_help ();
-			exit (1);
-		}
-
-		msg.type = UPSTART_WATCH_EVENTS;
-		expect_reply = -1;
-	} else if (! strcmp (args[0], "shutdown")) {
-		if (args[1] == NULL) {
-			fprintf (stderr, _("%s: missing argument\n"),
-				 program_name);
-			nih_main_suggest_help ();
-			exit (1);
-		}
-
-		msg.type = UPSTART_SHUTDOWN;
-		msg.shutdown.name = args[1];
-		expect_reply = 0;
+	} else if ((reply->job_status.process_state == PROCESS_SPAWNED)
+		   || (reply->job_status.process_state == PROCESS_NONE)) {
+		nih_message (", process %s\n",
+			     process_state_name (
+				     reply->job_status.process_state));
 	} else {
-		fprintf (stderr, _("%s: unknown command: %s\n"),
-			 program_name, args[0]);
-		nih_main_suggest_help ();
-		exit (1);
+		nih_message (", process %d %s\n", reply->job_status.pid,
+			     process_state_name (
+				     reply->job_status.process_state));
 	}
+}
 
-	/* Send the message */
-	if (upstart_send_msg (sock, &msg) < 0) {
-		NihError *err;
+/**
+ * print_event:
+ * @reply: message received.
+ *
+ * Output the name of an event in a notification received from the
+ * init daemon.
+ **/
+static void
+print_event (UpstartMsg *reply)
+{
+	nih_assert (reply != NULL);
+	nih_assert (reply->type == UPSTART_EVENT);
 
-		err = nih_error_get ();
-		nih_error (_("Unable to send message: %s"), err->message);
-		exit (1);
-	}
-
-	/* Listen for replies */
-	while (expect_reply) {
-		UpstartMsg *reply;
-
-		reply = upstart_recv_msg (NULL, sock, NULL);
-		if (! reply) {
-			NihError *err;
-
-			err = nih_error_get ();
-			nih_error (_("Error receiving message: %s"),
-				   err->message);
-			exit (1);
-		}
-
-		switch (reply->type) {
-		case UPSTART_JOB_STATUS:
-			printf ("%s (%s) %s", reply->job_status.name,
-				job_goal_name (reply->job_status.goal),
-				job_state_name (reply->job_status.state));
-
-			if (reply->job_status.state == JOB_WAITING) {
-				printf ("\n");
-
-			} else if ((reply->job_status.process_state == PROCESS_SPAWNED)
-				   || (reply->job_status.process_state == PROCESS_NONE)) {
-				printf (", process %s\n",
-					process_state_name (reply->job_status.process_state));
-			} else {
-				printf (", process %d %s\n",
-					reply->job_status.pid,
-					process_state_name (reply->job_status.process_state));
-			}
-			break;
-		case UPSTART_JOB_UNKNOWN:
-			fprintf (stderr, _("%s: Unknown job: %s\n"),
-				 program_name, reply->job_unknown.name);
-			break;
-		case UPSTART_JOB_LIST_END:
-			expect_reply = 0;
-			break;
-		case UPSTART_EVENT:
-			printf ("%s event\n", reply->event.name);
-			break;
-		default:
-			fprintf (stderr, _("%s: Unexpected reply (type %d)\n"),
-				 program_name, reply->type);
-		}
-
-		if (expect_reply > 0)
-			expect_reply--;
-	}
-
-	return 0;
+	nih_message (_("%s event"), reply->event.name);
 }
