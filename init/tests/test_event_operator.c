bdcf3be58b09bf70b3f0dcfa152f4d57caa661c9
Scott James Remnant <scott@netsplit.com>
2010-02-04 00:33:40 -0800

    Bump copyright years

--- init/tests/test_event_operator.c
+++ init/tests/test_event_operator.c
@@ -2,7 +2,7 @@
  *
  * test_event_operator.c - test suite for init/event_operator.c
  *
- * Copyright © 2009 Canonical Ltd.
+ * Copyright © 2010 Canonical Ltd.
  * Author: Scott James Remnant <scott@netsplit.com>.
  *
  * This program is free software; you can redistribute it and/or modify
