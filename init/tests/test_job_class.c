851ae4c712f0e943286b70e271ccd33361dd611a
Marc A. Dahlhaus <mad@wol.de>
2011-05-05 12:26:00 +0200

    Add a missing job_class check for kill_signals default value.

--- init/tests/test_job_class.c
+++ init/tests/test_job_class.c
@@ -121,6 +121,7 @@ test_new (void)
 		TEST_EQ (class->task, FALSE);
 
 		TEST_EQ (class->kill_timeout, 5);
+		TEST_EQ (class->kill_signal, SIGTERM);
 
 		TEST_EQ (class->respawn, FALSE);
 		TEST_EQ (class->respawn_limit, 10);
