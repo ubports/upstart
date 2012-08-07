5a7b46ee4100330594eeadc525845013d4f8a66f
Steve Langasek <steve.langasek@canonical.com>
2012-08-06 18:26:29 -0700

    Micro-optimization: order our device node checks by which ones are actually most likely to be absent in the case when we have a partially-populated static /dev

--- init/main.c
+++ init/main.c
@@ -247,12 +247,12 @@ main (int   argc,
 		/* Check if key devices already exist; if they do,
 		 * we should assume we don't need to mount /dev.
 		 */
-		if (system_check_file ("/dev/null", S_IFCHR, makedev (1, 3)) < 0
-		    || system_check_file ("/dev/console", S_IFCHR, makedev (5, 1)) < 0
-		    || system_check_file ("/dev/tty", S_IFCHR, makedev (5, 0)) < 0
+		if (system_check_file ("/dev/ptmx", S_IFCHR, makedev (5, 2)) < 0)
+		    || system_check_file ("/dev/pts", S_IFDIR, 0) < 0
 		    || system_check_file ("/dev/kmsg", S_IFCHR, makedev (1, 11)) < 0
-		    || system_check_file ("/dev/ptmx", S_IFCHR, makedev (5, 2)) < 0)
-		    || system_check_file ("/dev/pts", S_IFDIR, 0) < 0)
+		    || system_check_file ("/dev/null", S_IFCHR, makedev (1, 3)) < 0
+		    || system_check_file ("/dev/console", S_IFCHR, makedev (5, 1)) < 0
+		    || system_check_file ("/dev/tty", S_IFCHR, makedev (5, 0)) < 0)
 			needs_devtmpfs = 1;
 
 		if (needs_devtmpfs) {
