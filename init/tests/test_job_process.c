f1aa7a014120bb232a75eaa7b7ec29ea33a648c7
Dimitri John Ledkov <dimitri.ledkov@canonical.com>
2014-07-10 16:56:48 +0100

    Back out more of -r1647

--- init/tests/test_job_process.c
+++ init/tests/test_job_process.c
@@ -5853,7 +5853,6 @@ test_handler (void)
 		job->failed = FALSE;
 		job->failed_process = PROCESS_INVALID;
 		job->exit_status = 0;
-		job->process_data[PROCESS_PRE_START]->valid = FALSE;
 
 		job_process_handler (NULL, 1, NIH_CHILD_EXITED, 0);
 		TEST_EQ (job->state, JOB_SPAWNED);
